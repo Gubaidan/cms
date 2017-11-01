711ba0ff613ec68d83e8402ba467909eee20efaa
night <492407268@qq.com>
2017-11-01 12:00:00 +0800

    修改 bootstrap-datetimepicker.ro

--- target/shishuocms-2.0.1/WEB-INF/static/manage/assets/bootstrap.datetimepicker/js/locales/bootstrap-datetimepicker.ro.js
+++ target/shishuocms-2.0.1/WEB-INF/static/manage/assets/bootstrap.datetimepicker/js/locales/bootstrap-datetimepicker.ro.js
@@ -9,6 +9,7 @@
 		daysMin: ["Du", "Lu", "Ma", "Mi", "Jo", "Vi", "Sâ", "Du"],
 		months: ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"],
 		monthsShort: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		monthsShort: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 		today: "Astăzi",
 		suffix: [],
 		meridiem: [],
